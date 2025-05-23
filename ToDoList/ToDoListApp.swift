87d40bce4632fbbbd38879ea694fac7eea21e5b5
Pedro Henrique Nascimento Lemos <87588543+Pedro-Lemos@users.noreply.github.com>
2025-05-23 00:14:55 -0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Pedro Lemos on 23/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
